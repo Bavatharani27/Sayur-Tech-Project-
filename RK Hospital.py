0446f7e19ffb32640eb9a98e66f870556e0f1809
Bavatharani <74584793+Bavatharani27@users.noreply.github.com>
2022-12-19 00:23:23 +0530

    Update RK Hospital.py

--- RK Hospital.py
+++ RK Hospital.py
@@ -31,13 +31,21 @@ def login():
 def get_login_page():
     return render_template("LoginPage.html")
 
-@app.route('/doctorlogin')
+@app.route('/doctorlogin')                                #Doctor Login
 def get_doctor_login_page():
     return render_template("DoctorLogin.html")
+
+@app.route('/nurselogin')                                  #Nurse Login
+def get_nurse_login_page():
+    return render_template("NurseLogin.html")
 
 @app.route('/doctorpage')
 def get_doctor_page():
     return render_template("DoctorPage.html")
+
+@app.route('/nursepage')
+def get_nurse_page():
+    return render_template("NursePage.html")
 
 @app.route('/admin')
 def get_admin_page():
@@ -110,11 +118,10 @@ def get_patient_list():
         patient.append(output)
     return patient 
 
-@app.route("/assignedpatientlist/<int:DoctorId>")
+@app.route("/assignedpatientlist/<int:DoctorId>")              #Doctor Activity - Patient Name populate
 def get_assigned_patient_list(DoctorId):  
     patients = [] 
     logged_doctor_Id = ''
-    #DoctorId  = request.form.get('DoctorId')
     
     DocId = "select int_Doctor_Id from tbl_doctor where int_User_Id = " + str(DoctorId)
     mycursor.execute(DocId)
@@ -133,6 +140,37 @@ def get_assigned_patient_list(DoctorId):
                 'name': i[1]}
         patients.append(output)
     return patients 
+
+@app.route('/doctoractivitysave', methods=["POST"])       #Doctor Activity Save
+def create_new_activity_for_doctor():
+    global doctor_activity
+    logged_doctor_Id = ''
+    
+    DocId = request.form.get('DocId')
+    
+    DoctorId = "select int_Doctor_Id from tbl_doctor where int_User_Id = " + str(DocId)
+    mycursor.execute(DoctorId)
+    myresult = mycursor.fetchone()
+    for i in myresult:
+        logged_doctor_Id = str(i)
+    
+    CurrentDt = request.form.get('CurrentDt')
+    PatientId = request.form.get('PatientId')
+    AttendFlag = request.form.get('AttendFlag')
+    Duration = request.form.get('Duration')
+    
+    doctor_activity = [(logged_doctor_Id, CurrentDt, PatientId, AttendFlag, Duration)]
+    stmt = "Insert into tbl_doctoractivity (Doctor_Id, Activity_Date, Patient_Id, Attend_Flag, Duration) values(%s, %s, %s, %s, %s)"
+    mycursor.executemany(stmt, doctor_activity)
+    
+    updatestmt = "update tbl_shiftpatientfordoctor set tbl_shiftpatientfordoctor.attended_Flag = " + AttendFlag + "\
+        where tbl_shiftpatientfordoctor.Patient_Id = " + PatientId
+        
+    print(updatestmt)
+    mycursor.execute(updatestmt)
+    
+    mydb.commit()
+    return ''
 
 @app.route('/assignShiftfordoctor', methods=["POST"])     
 def create_new_shift_for_doctor():
@@ -197,7 +235,7 @@ def get_all_doctorshift():
 def load_doctor_list():
     return render_template("doctorShiftAssign.html")
 
-@app.route('/loaddoctordetails', methods=["POST","GET"])                          
+@app.route('/loaddoctordetails', methods=["POST","GET"])                 #load doctor shift details         
 def get_doctor_detail():
     doc_details = []
     logged_doctor_Id = ''
@@ -233,6 +271,53 @@ def get_doctor_detail():
        
     return doc_details
 
+@app.route('/loaddoctoractivitydetails', methods=["POST","GET"])                 #load doctor activity details         
+def get_doctor_activity_detail():
+    doc_activity_details = []
+    logged_doctor_Id = ''
+    DoctorId  = request.form.get('DoctorId')
+    CurrentDt   = request.form.get('CurrentDt') 
+    PatientId   = request.form.get('PatientId') 
+    AttendFlag   = request.form.get('AttendFlag') 
+    #Duration   = request.form.get('Duration') 
+    
+    DocId = "select int_Doctor_Id from tbl_doctor where int_User_Id = " + DoctorId
+    mycursor.execute(DocId)
+    myresult = mycursor.fetchone()
+    for i in myresult:
+        logged_doctor_Id = str(i)
+        
+    main_query = "select d.txt_Doctor_Name, da.Activity_Date, p.txt_Patient_Name, \
+                  case when da.Attend_Flag = '2' then 'No' when da.Attend_Flag = '1' then 'Yes' end as AttendedFlag \
+                     from tbl_doctoractivity da, tbl_doctor d, tbl_patient p \
+                     where d.int_Doctor_Id = da.Doctor_Id and p.int_Patient_Id = da.Patient_Id"
+           
+    doctor_clause = ''
+    date_clause = ''
+    patient_clause = ''
+    flag_clause = ''
+             
+    if(logged_doctor_Id != '0' and logged_doctor_Id != ''):
+        doctor_clause = ' and d.int_Doctor_Id = '+ logged_doctor_Id 
+    
+    if(CurrentDt != '--' and CurrentDt != ''):
+        date_clause = ' and da.Activity_Date = "'+ CurrentDt + '"'
+        
+    if(PatientId != '0' and PatientId != ''):
+        patient_clause = ' and p.int_Patient_Id = '+ PatientId 
+        
+    if(AttendFlag != '0' and AttendFlag != ''):
+        flag_clause = ' and da.Attend_Flag = '+ AttendFlag 
+        
+    final_query = main_query + doctor_clause + date_clause + patient_clause + flag_clause
+    print(final_query)
+    mycursor.execute(final_query)
+    myresult = mycursor.fetchall()
+    for i in myresult:
+        doc_activity_details.append(i)
+       
+    return doc_activity_details
+
 @app.route("/getnurselist")
 def get_nurse_shift_list():  
     nurse = [] 
@@ -244,6 +329,118 @@ def get_nurse_shift_list():
                 'name': i[1]}
         nurse.append(output)
     return nurse    
+
+@app.route('/loadnursedetails', methods=["POST","GET"])                 #load nurse shift details         
+def get_nurse_detail():
+    nurse_details = []
+    logged_nurse_Id = ''
+    NurseId  = request.form.get('NurseId')
+    ShiftDt   = request.form.get('ShiftDt') 
+    
+    NurId = "select int_Nurse_Id from tbl_nurse where int_User_Id = " + NurseId
+    mycursor.execute(NurId)
+    myresult = mycursor.fetchone()
+    for i in myresult:
+        logged_nurse_Id = str(i)
+        
+    main_query = "select n.txt_Nurse_Name, s.txt_Shift_Type, sn.Shift_Date from tbl_shiftfornurse sn, tbl_nurse n, tbl_shift s \
+                     where n.int_Nurse_Id = sn.Nurse_Id and sn.Shift_Id = s.int_Shift_Id"
+           
+    nurse_clause = ''
+    date_clause = ''
+             
+    if(logged_nurse_Id != '0' and logged_nurse_Id != ''):
+        nurse_clause = ' and n.int_Nurse_Id = '+ logged_nurse_Id 
+    
+    if(ShiftDt != '--' and ShiftDt != ''):
+        date_clause = ' and sn.Shift_Date = "'+ ShiftDt + '"'
+        
+    final_query = main_query + nurse_clause + date_clause 
+    print(final_query)
+    mycursor.execute(final_query)
+    myresult = mycursor.fetchall()
+    for i in myresult:
+        nurse_details.append(i)
+       
+    return nurse_details
+
+@app.route('/nurseactivitysave', methods=["POST"])       #Nurse Activity Save
+def create_new_activity_for_nurse():
+    nurse_activity = ''
+    logged_nurse_Id = ''
+    
+    NurId = request.form.get('NurseId')
+    
+    NurseId = "select int_Nurse_Id from tbl_nurse where int_User_Id = " + str(NurId)
+    mycursor.execute(NurseId)
+    myresult = mycursor.fetchone()
+    for i in myresult:
+        logged_nurse_Id = str(i)
+    
+    CurrentDt = request.form.get('CurrentDt')
+    PatientId = request.form.get('PatientId')
+    AttendFlag = request.form.get('AttendFlag')
+    Duration = request.form.get('Duration')
+    
+    nurse_activity = [(logged_nurse_Id, CurrentDt, PatientId, AttendFlag, Duration)]
+    stmt = "Insert into tbl_nurseactivity (Nurse_Id, Activity_Date, Patient_Id, Attend_Flag, Duration) values(%s, %s, %s, %s, %s)"
+    mycursor.executemany(stmt, nurse_activity)
+    
+    # updatestmt = "update tbl_shiftfornurse set tbl_shiftfornurse.attended_Flag = " + AttendFlag + "\
+    #     where tbl_shiftfornurse.Patient_Id = " + PatientId
+        
+    # print(updatestmt)
+    # mycursor.execute(updatestmt)
+    
+    mydb.commit()
+    return ''
+
+@app.route('/loadnurseactivitydetails', methods=["POST","GET"])                 #load nurse activity details         
+def get_nurse_activity_detail():
+    nurse_activity_details = []
+    logged_nurse_Id = ''
+    NurseId  = request.form.get('NurseId')
+    CurrentDt   = request.form.get('CurrentDt') 
+    PatientId   = request.form.get('PatientId') 
+    AttendFlag   = request.form.get('AttendFlag') 
+    #Duration   = request.form.get('Duration') 
+    
+    NurId = "select int_Nurse_Id from tbl_nurse where int_User_Id = " + NurseId
+    mycursor.execute(NurId)
+    myresult = mycursor.fetchone()
+    for i in myresult:
+        logged_nurse_Id = str(i)
+        
+    main_query = "select n.txt_Nurse_Name, na.Activity_Date, p.txt_Patient_Name, \
+                  case when na.Attend_Flag = '2' then 'No' when na.Attend_Flag = '1' then 'Yes' end as AttendedFlag, na.Duration \
+                     from tbl_nurseactivity na, tbl_nurse n, tbl_patient p \
+                     where n.int_Nurse_Id = na.Nurse_Id and p.int_Patient_Id = na.Patient_Id"
+           
+    nurse_clause = ''
+    date_clause = ''
+    patient_clause = ''
+    flag_clause = ''
+             
+    if(logged_nurse_Id != '0' and logged_nurse_Id != ''):
+        nurse_clause = ' and n.int_Nurse_Id = '+ logged_nurse_Id 
+    
+    if(CurrentDt != '--' and CurrentDt != ''):
+        date_clause = ' and na.Activity_Date = "'+ CurrentDt + '"'
+        
+    if(PatientId != '0' and PatientId != ''):
+        patient_clause = ' and p.int_Patient_Id = '+ PatientId 
+        
+    if(AttendFlag != '0' and AttendFlag != ''):
+        flag_clause = ' and na.Attend_Flag = '+ AttendFlag 
+        
+    final_query = main_query + nurse_clause + date_clause + patient_clause + flag_clause
+    print(final_query)
+    mycursor.execute(final_query)
+    myresult = mycursor.fetchall()
+    for i in myresult:
+        nurse_activity_details.append(i)
+       
+    return nurse_activity_details
 
 @app.route('/assignShiftfornurse', methods=["POST"])     
 def create_new_shift_for_nurse():
